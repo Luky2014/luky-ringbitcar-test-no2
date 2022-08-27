715899ba5e8c22848df8a19f7516b691b68a8c22
Luky2014 <111812835+Luky2014@users.noreply.github.com>
2022-08-27 15:04:26 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,19 @@
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    basic.showString("Hello Luky, i am Microbit!")
+    basic.showLeds(`
+        # . # # .
+        # # # # .
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    music.setVolume(60)
+    music.playMelody("G G A - A G F B ", 150)
+    music.playMelody("B A - A G F B B ", 150)
+    music.playMelody("A - A G A G G G ", 150)
+})
 input.onButtonPressed(Button.A, function () {
-    RingbitCar.freestyle(100, 50)
-    RingbitCar.freestyle(100, 40)
-    RingbitCar.freestyle(110, 30)
-    RingbitCar.freestyle(120, 20)
-    RingbitCar.freestyle(150, 10)
+    RingbitCar.freestyle(125, 25)
     RingbitCar.brake()
 })
 input.onButtonPressed(Button.AB, function () {
@@ -14,8 +24,11 @@ input.onButtonPressed(Button.AB, function () {
 })
 input.onButtonPressed(Button.B, function () {
     RingbitCar.freestyle(25, 125)
+    basic.pause(500)
     RingbitCar.running_distance(RingbitCar.Direction_run.backward, 20)
+    basic.pause(500)
     RingbitCar.turnleft()
+    basic.pause(500)
     RingbitCar.running_distance(RingbitCar.Direction_run.forward, 15)
     RingbitCar.brake()
 })
@@ -23,18 +36,6 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     RingbitCar.running_distance(RingbitCar.Direction_run.backward, 5)
 })
 let strip = neopixel.create(DigitalPin.P0, 2, NeoPixelMode.RGB)
-basic.showString("Hello Luky, i am Microbit!")
-basic.showLeds(`
-    # . # # .
-    # # # # .
-    # # # # #
-    # # # # #
-    . # # # .
-    `)
-music.setVolume(60)
-music.playMelody("G G A - A G F B ", 150)
-music.playMelody("B A - A G F B B ", 150)
-music.playMelody("A - A G A G G G ", 150)
 RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
 basic.forever(function () {
     strip.showColor(neopixel.colors(NeoPixelColors.Red))
